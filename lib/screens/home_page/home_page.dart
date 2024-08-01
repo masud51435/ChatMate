1a98e912a54b84ff2ba809d54ab9dcfcafa3e9bc
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-08-01 21:53:57 +0600

    added bottom field

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -5,6 +5,50 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChatMate'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const Expanded(
+              child: Column(
+                children: [],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      hintText: 'Message ChatMate',
+                      filled: true,
+                      fillColor: Colors.grey.shade300,
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(40),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.send,
+                    size: 30,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

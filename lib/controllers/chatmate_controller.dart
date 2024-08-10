e62f40f3435e286c1fb1d3110ac8e9b7938fca7a
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-08-11 01:17:55 +0600

    added image and set up chat look

--- lib/controllers/chatmate_controller.dart
+++ lib/controllers/chatmate_controller.dart
@@ -1,5 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ChatmateController extends GetxController {
   static ChatmateController get instance => Get.find();
+
+  final TextEditingController controller = TextEditingController();
+
+  final List<Message> messages = [
+    Message(text: 'Hello, how are you?', isUser: true),
+    Message(text: 'I\'m doing well, thank you!', isUser: false),
+    Message(text: 'How about you?', isUser: true),
+    Message(text: 'I\'m also doing well. What about you?', isUser: false),
+    Message(text: 'What is your name?', isUser: true),
+    Message(text: 'My name is gemini Ai, what is your name?', isUser: false),
+    // Add more messages as needed...
+  ];
+}
+
+class Message {
+  Message({
+    required this.text,
+    required this.isUser,
+  });
+  final String text;
+  final bool isUser;
 }
--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,5 +1,6 @@
+import 'package:chatmate/controllers/chatmate_controller.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import 'appbar.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +8,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ChatmateController controller = Get.put(ChatmateController());
     return Scaffold(
       appBar: const HomeAppBar(),
       drawer: const Drawer(),
@@ -14,9 +16,44 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            const Expanded(
-              child: Column(
-                children: [],
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final message = controller.messages[index];
+                  return ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 0,
+                      vertical: 2,
+                    ),
+                    title: Align(
+                      alignment: message.isUser
+                          ? Alignment.topRight
+                          : Alignment.topLeft,
+                      child: Container(
+                        margin: message.isUser
+                            ? const EdgeInsets.only(left: 60)
+                            : EdgeInsets.zero,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: message.isUser
+                              ? const Border()
+                              : Border.all(color: Colors.grey),
+                          color: message.isUser
+                              ? Colors.blue.shade400
+                              : Colors.transparent,
+                        ),
+                        child: Text(
+                          message.text,
+                          style: TextStyle(
+                            color: message.isUser ? Colors.white : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
             Row(
@@ -38,7 +75,8 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                IconButton(
+                const SizedBox(width: 5),
+                IconButton.outlined(
                   onPressed: () {},
                   icon: Icon(
                     Icons.send,
